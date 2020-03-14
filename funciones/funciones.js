4d5a7c1c02f64e3c2939e7e4bc7002a109d97bed
mavo97 <mavodeveloper@gmail.com>
2020-03-14 16:57:34 -0600

    repository

---
+++ funciones/funciones.js
@@ -0,0 +1,11 @@
+function copyUrl() {
+    var copyText = document.getElementById("url");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+    console.log('ss');
+}
